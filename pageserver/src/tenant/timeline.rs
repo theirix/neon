e1ba3de26b0e4c13bb68bc8efb5ee9bbbd5e8fc2
Christian Schwarz <christian@neon.tech>
2022-12-06 10:27:26 -0500

    fixup: on-demand download: physical size metric (impl & unit tests)

--- pageserver/src/tenant/timeline.rs
+++ pageserver/src/tenant/timeline.rs
@@ -1110,7 +1110,6 @@ impl Timeline {
                         &remote_layer_metadata,
                     );
 
-                    // FIXME: when to update physical size?
                     self.layers
                         .write()
                         .unwrap()
@@ -1137,7 +1136,6 @@ impl Timeline {
                         &remote_layer_metadata,
                     );
 
-                    // FIXME: when to update physical size?
                     self.layers
                         .write()
                         .unwrap()
@@ -2806,7 +2804,11 @@ impl Timeline {
                                 &remote_layer.file_name,
                                 &remote_layer.layer_metadata,
                             )
-                            .await;
+                            .await; // FIXME(christian) error handling???
+
+                        if let Ok(size) = result {
+                            self.metrics.current_physical_size_gauge.add(size);
+                        }
 
                         // Download complete. Replace the RemoteLayer with the corresponding
                         // Delta- or ImageLayer in the layer map.
