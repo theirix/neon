1d96be18a799f345b5529cb0148a09d0e7fe9e7d
Christian Schwarz <christian@neon.tech>
2022-12-07 12:51:51 -0500

    fixup: on-demand download: minor clippy complaints

--- pageserver/src/tenant/timeline.rs
+++ pageserver/src/tenant/timeline.rs
@@ -1105,7 +1105,7 @@ impl Timeline {
                     let remote_layer = RemoteLayer::new_img(
                         self.tenant_id,
                         self.timeline_id,
-                        &imgfilename,
+                        imgfilename,
                         &remote_layer_metadata,
                     );
 
@@ -1131,7 +1131,7 @@ impl Timeline {
                     let remote_layer = RemoteLayer::new_delta(
                         self.tenant_id,
                         self.timeline_id,
-                        &deltafilename,
+                        deltafilename,
                         &remote_layer_metadata,
                     );
 
