da163776e7d7c515d9e3c1c154f1aa2134d66551
Christian Schwarz <christian@neon.tech>
2022-12-08 11:05:26 -0500

    Timeline::download_remote_layer: make it return the downloaded size in the result
    Will be used in the next patch.

--- pageserver/src/tenant/timeline.rs
+++ pageserver/src/tenant/timeline.rs
@@ -1498,7 +1498,7 @@ impl Timeline {
                     let future = tl.download_remote_layer(Arc::clone(&remote_layer));
 
                     let dynfuture: std::pin::Pin<
-                        Box<dyn Future<Output = anyhow::Result<()>> + Send + Sync>,
+                        Box<dyn Future<Output = anyhow::Result<u64>> + Send + Sync>,
                     > = Box::pin(future);
                     return Err(PageReconstructError::NeedDownload(dynfuture));
                 }
@@ -2762,7 +2762,7 @@ impl Timeline {
     pub async fn download_remote_layer(
         self: Arc<Self>,
         remote_layer: Arc<RemoteLayer>,
-    ) -> anyhow::Result<()> {
+    ) -> anyhow::Result<u64> {
         let s = Arc::clone(&self);
         let remote_layer = Arc::clone(&remote_layer);
 
@@ -2776,7 +2776,7 @@ impl Timeline {
                 info!("download has already started, waiting");
                 sender.subscribe()
             } else {
-                let (sender, receiver) = tokio::sync::watch::channel(Ok(()));
+                let (sender, receiver) = tokio::sync::watch::channel(None);
                 *download_watch = Some(sender);
 
                 // Need to spawn, because the future returned by download_layer_file is
@@ -2816,8 +2816,7 @@ impl Timeline {
 
                         // Notify waiters that the download has finished.
                         if let Some(sender) = remote_layer.download_watch.lock().unwrap().as_ref() {
-                            let result = result.map(|_| ()); // download_watch doesn't care about size
-                            let _ = sender.send_replace(result);
+                            let _ = sender.send_replace(Some(result));
                         }
                         Ok(())
                     },
@@ -2834,18 +2833,21 @@ impl Timeline {
         // We cannot return the Result we received directly, because it does not
         // implement Clone. We need to construct a new Error from it.
         let x = receiver.borrow();
-        if let Err(err) = x.as_ref() {
-            // This error lands in the PageReconstructError future and might be bubbled far up the call stack.
-            // Repeat fully identifying information here.
-            bail!(
-                "could not download layer file tenant={} timeline={} layer={}: {:?}",
-                s.tenant_id,
-                s.timeline_id,
-                remote_layer.short_id(),
-                err
-            );
+        match x.as_ref() {
+            None => unreachable!("sender only updates the watch once with Some()"),
+            Some(Err(err)) => {
+                // This error lands in the PageReconstructError future and might be bubbled far up the call stack.
+                // Repeat fully identifying information here.
+                bail!(
+                    "could not download layer file tenant={} timeline={} layer={}: {:?}",
+                    s.tenant_id,
+                    s.timeline_id,
+                    remote_layer.short_id(),
+                    err
+                );
+            }
+            Some(Ok(sz)) => Ok(*sz),
         }
-        Ok(())
     }
 }
 
@@ -2863,7 +2865,7 @@ pub enum PageReconstructError {
     /// probably succeed if you retry it. (It can return NeedDownload again, if
     /// another layer needs to be downloaded.).
     #[error("layer file needs to be downloaded")]
-    NeedDownload(std::pin::Pin<Box<dyn Future<Output = anyhow::Result<()>> + Send + Sync>>),
+    NeedDownload(std::pin::Pin<Box<dyn Future<Output = anyhow::Result<u64>> + Send + Sync>>),
 }
 
 impl std::fmt::Debug for PageReconstructError {
