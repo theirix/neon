f5d679f118db7107beb043038eb9a2a2af2a34e8
Christian Schwarz <christian@neon.tech>
2022-12-09 11:27:56 -0500

    add FIXME to remind ourselves that we need to schedule pending deletions if a layer file fails to delete

--- pageserver/src/tenant/timeline.rs
+++ pageserver/src/tenant/timeline.rs
@@ -2779,7 +2779,7 @@ impl Timeline {
                         .sub(path.metadata()?.len());
                 }
                 layer_names_to_delete.push(doomed_layer.filename());
-                doomed_layer.delete()?;
+                doomed_layer.delete()?; // FIXME: schedule succeeded deletions before returning?
                 layers.remove_historic(doomed_layer);
                 result.layers_removed += 1;
             }
